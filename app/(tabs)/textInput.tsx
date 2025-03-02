92b3444d40aa20007c70585f24d83b11bbead0e7
longhoanglekim <longngerr@gmail.com>
2025-03-03 00:12:53 +0700

    delete conflict

--- app/(tabs)/textInput.tsx
+++ app/(tabs)/textInput.tsx
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-// app/textInput.tsx
 import { useState } from "react";
 import { View, TextInput } from "react-native";
 
@@ -34,40 +32,3 @@ export default function TextInputScreen() {
     </View>
   );
 }
-=======
-// app/textInput.tsx
-import { useState } from "react";
-import { View, TextInput } from "react-native";
-
-export default function TextInputScreen() {
-  const [textValue, changeTextValue] = useState("");
-  const [passwordValue, changePasswordValue] = useState("");
-  return (
-    <View style={{}}>
-      <TextInput
-        style={{ backgroundColor: "lightblue", padding: 10, borderWidth: 1 }}
-        value={textValue}
-        onChangeText={(value) => {
-          changeTextValue(value);
-        }}
-        placeholder="Please enter your name..."
-        keyboardType={"default"} // default, email,numeric
-        returnKeyType={"next"}
-        autoFocus={false}
-      />
-      <TextInput
-        style={{ backgroundColor: "lightblue", padding: 10, borderWidth: 1 }}
-        value={passwordValue}
-        onChangeText={(value) => {
-          changePasswordValue(value);
-        }}
-        secureTextEntry={true}
-        keyboardType={"default"}
-        placeholder="Please enter your password..."
-        returnKeyType={"done"}
-        autoFocus={false}
-      />
-    </View>
-  );
-}
->>>>>>> c27444aea867ed87574263e841300179355e1d38
